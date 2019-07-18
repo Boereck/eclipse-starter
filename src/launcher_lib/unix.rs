ffe4d2ad01c3dcffc3cf53da908067ea60304a18
Max Bureck <max.bureck@fokus.fraunhofer.de>
2019-07-18 16:56:31 +0200

    Linux build now feature identical with Windows build

--- src/launcher_lib/unix.rs
+++ src/launcher_lib/unix.rs
@@ -33,7 +33,28 @@ impl<'a,'b> EclipseLauncher<'a,'b> for EclipseLauncherNix<'a>
 	}
 
 	fn run<S: AsRef<str>>(&self, args: &[S], vm_args: &[S]) -> Result<(), String> {
-		unimplemented!()
+		let count_args = args.len() as c_int;
+
+		// convert args to native types
+		let args_vec_cstring = str_slice_to_cstring_vec(args);
+		let args_vec_nativestring = cstring_vec_to_nativestr_vec(&args_vec_cstring);
+		let args_ptr_nativestring = args_vec_nativestring.as_ptr();
+
+		// convert vm_args to native types
+		let vm_args_vec_cstring = str_slice_to_cstring_vec(vm_args);
+		let mut vm_args_vec_nativestring = cstring_vec_to_nativestr_vec(&vm_args_vec_cstring);
+		// VM args are null-terminated, so we need to add a trailing null
+		vm_args_vec_nativestring.push(std::ptr::null());
+		let vm_args_ptr_nativestring = vm_args_vec_nativestring.as_ptr();
+		unsafe {
+			let result = (self.lib_api.run)(count_args, args_ptr_nativestring, vm_args_ptr_nativestring);
+			if result == 0 {
+				Ok(())
+			} else {
+				// TODO: handle error codes?
+				Err("TODO: something went wrong!".into())
+			}
+		}
 	}
 
 	fn set_initial_args(&self, params: &SetInitialArgsParams) -> Result<(), String> {
@@ -62,14 +83,8 @@ pub struct SetInitialArgsParams<'a> {
 
 impl<'b> InitialArgs<'b> for SetInitialArgsParams<'b> {
 	fn new<S: AsRef<str>>(args: &'b [S], library: &'b str) -> Self {
-		let args_vec_cstring_param = args
-			.iter()
-			.filter_map(|s| CString::new(s.as_ref()).ok())
-			.collect::<Vec<CString>>();
-		let args_vec_nativestr_param = args_vec_cstring_param
-			.iter()
-			.map(|s| s.as_ptr())
-			.collect::<Vec<NativeString>>();
+		let args_vec_cstring_param = str_slice_to_cstring_vec(args);
+		let args_vec_nativestr_param = cstring_vec_to_nativestr_vec(&args_vec_cstring_param);
 		let args_ptr_nativestr_param = args_vec_nativestr_param.as_ptr();
 
 		let library_cstring_param = CString::new(library).unwrap_or_default();
@@ -84,4 +99,17 @@ impl<'b> InitialArgs<'b> for SetInitialArgsParams<'b> {
 			phantom: PhantomData,
 		}
 	}
+}
+
+fn str_slice_to_cstring_vec<S: AsRef<str>>(args: &[S]) -> Vec<CString> {
+	args
+		.iter()
+		.filter_map(|s| CString::new(s.as_ref()).ok())
+		.collect()
+}
+
+fn cstring_vec_to_nativestr_vec(args: &Vec<CString>) -> Vec<NativeString> {
+	args.iter()
+		.map(|s| s.as_ptr())
+		.collect()
 }
